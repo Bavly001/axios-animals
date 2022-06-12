460cd52403924740173b3dbfcd0d11ceb5a36451
unknown <a_bavly@yahoo.com>
2022-06-12 21:06:53 +0200

    adding review route with its js

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 /*-----------------------Global Variables--------------------*/
+/*---------Image Variables---------*/
+let male = '/Images/avatar-male.png';
+let female = '/Images/female.png';
 
+/*-----------------------------------------------------------*/
 let page = document.body.id; //variable to identify the route 
 
 let dog = "https://dog.ceo/api/breeds/image/random";
@@ -56,7 +60,7 @@ if (page == "home") {
 }
 
 /*---------Animals Router---------*/
-if (page == "fox-page") {
+else if (page == "fox-page") {
     img_id = "foxes-background";
     axios.get(fox).then(res => image_view(res.data.image));
     setInterval(
@@ -66,3 +70,73 @@ if (page == "fox-page") {
     );
 }
 
+
+/*---------Review Router---------*/
+else {
+    // Adding variables
+    let reviews_number = document.getElementsByClassName("review");
+    document.getElementById("number").innerHTML = "(" + reviews_number.length + ")";
+
+    let first_name = document.getElementById("first-name");
+    let last_name = document.getElementById("last-name");
+    let email = document.getElementById("email");
+    let gender = document.getElementsByName("gender");
+    let comment = document.getElementById("comment");
+    let submit = document.getElementById("submit");
+    let error_text = document.getElementById("error")
+
+    let reviews_section = document.getElementById("reviews");
+
+    // Change Reviews number
+
+    submit.addEventListener("click", function (e) {
+        e.preventDefault();
+
+        if (first_name.value != "" && last_name.value != "" && email.value != "" && (gender[0].checked || gender[1].checked) && comment.value != "") {
+            if (error_text.style.display == "block") {
+                error_text.style.display = "none";
+            }
+            const info = document.createElement("div");
+            info.className = "personal-info center";
+
+            const avatar_image = document.createElement("img");
+            avatar_image.className = "avatar";
+
+            const full_name = document.createElement("h2");
+            full_name.className = "full-name";
+
+            const review = document.createElement("div");
+            review.className = "review";
+
+            const review_comment = document.createElement("p");
+            review_comment.className = "comment";
+
+            if (gender[0].checked) {
+                avatar_image.src = male;
+            }
+            else {
+                avatar_image.src = female;
+            }
+
+            full_name.textContent = first_name.value + " " + last_name.value;
+            review_comment.textContent = comment.value;
+
+            info.appendChild(avatar_image);
+            info.appendChild(full_name);
+            review.appendChild(info);
+            review.appendChild(review_comment);
+
+            reviews_section.appendChild(review);
+            document.getElementById("number").innerHTML = "(" + reviews_number.length + ")";
+
+            const elements = document.getElementsByClassName("info");
+            Array.from(elements).forEach((element) => {element.value =""});
+            gender.forEach((e) => {e.checked = false});
+        }
+        else {
+            error_text.style.display = "block";
+        }
+    }
+    )
+
+}
